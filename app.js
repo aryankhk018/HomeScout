c398f61e6af258b17b1f8603c33025e408052441
Aryan Kaushik <aryankhk2460@gmail.com>
2026-06-13 00:32:13 +0530

    fixed mongodb error handler

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ const store = MongoStore.create({
   },
   touchAfter: 24 * 3600,
 });
-store.on("error", () => {
+store.on("error", (err) => {
   console.log("ERROR IN MONGO SESSION STORE", err);
 });
 
